4e93831b20783e45105ce3872bf57fba90e95bbf
Lucas Sant <lucasfdesantana095@gmail.com>
2026-01-22 23:54:16 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+import '../src/style/index.css'
+import ResultDiv from './components/ResultDiv'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ResultDiv/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ButtonNumber.jsx
@@ -0,0 +1,9 @@
+export default function ButtonNumber(props){
+    return(
+        <>
+            <button className='button-number'>
+                {props.number}
+            </button>
+        </>
+    )
+}
